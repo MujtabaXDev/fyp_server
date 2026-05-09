8730b9f4afad3a0b6db8f71934e3c08ced1d5956
MUHAMMAD MUJTABA <141523196+MujtabaXDev@users.noreply.github.com>
2026-05-09 23:29:54 +0500

    Change app.listen to bind to 0.0.0.0

--- index.js
+++ index.js
@@ -68,6 +68,6 @@ app.get("/", (req, res) => {
   res.send("Hello Foodi Client Server!");
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Example app listening on port ${port}`);
 });
